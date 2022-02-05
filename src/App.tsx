71284920e52e3838f648edbe214e20786ca30028
Yauheni Sauchanka <yauheni_sauchanka@epam.com>
2022-02-06 00:24:13 +0100

    work in progress

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <h1>
+            <span>
+                <img src={logo} className="App-logo" alt="logo" />
+            </span>
+            <span>
+              Work in progress ... Please come back later.
+            </span>
+          </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
