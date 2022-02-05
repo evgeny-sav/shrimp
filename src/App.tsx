33e9719b2f59b3a6c64a57fe9e2020bfbde06a04
Yauheni Sauchanka <yauheni_sauchanka@epam.com>
2022-02-06 00:28:06 +0100

    work in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -7,9 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
           <h1>
-            <span>
-                <img src={logo} className="App-logo" alt="logo" />
-            </span>
             <span>
               Work in progress ... Please come back later.
             </span>
